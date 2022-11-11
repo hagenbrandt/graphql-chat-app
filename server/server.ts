fe3726a58b176bb43dacc9077d80cf160fca2901
hbrandt <h.brandt@steinberg.de>
2022-11-11 15:09:27 +0100

    feat: Add http server to server define subscriptions to apollo server

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,16 @@
-import 'reflect-metadata'
-import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
-import { buildSchema } from 'type-graphql'
-import cors from 'cors'
+import { ApolloServer } from 'apollo-server-express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import http from 'http'
+import 'reflect-metadata'
+import { buildSchema } from 'type-graphql'
 import { ChatResolver } from './src/resolvers/chat'
 
 dotenv.config()
 
 const app = express()
+const httpServer = http.createServer(app)
 
 app.use(cors({ origin: `http://localhost:8080`, credentials: true }))
 
@@ -18,17 +20,27 @@ export const start = async () => {
       resolvers: [ChatResolver],
       validate: false,
     }),
+    subscriptions: {
+        path: '/subscriptions',
+        onConnect: () => {
+            console.log('Client connected for subscriptions');
+        },
+        onDisconnect: () => {
+            console.log('Client disconnected from subscriptions');
+        }
+    }
   })
 
   apolloServer.applyMiddleware({
     app,
     cors: false,
   })
+  apolloServer.installSubscriptionHandlers(httpServer)
 
-  // export const start = () => {
-  app.listen(process.env.PORT, () => {
+  httpServer.listen(process.env.PORT, () => {
     console.log(
-      `Server listen on port ${process.env.PORT} - ${apolloServer.graphqlPath}`
+      `Server listen on port http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`
     )
+    console.log(`Subscriptions ready at ws://localhost:${process.env.PORT}${apolloServer.subscriptionsPath}`);
   })
 }
