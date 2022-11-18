57e97be647c0395b76d1ba168d142d59f6f3d483
hbrandt <h.brandt@steinberg.de>
2022-11-18 23:38:55 +0100

    build: Install stylelint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,9 @@ import { WebSocketLink } from '@apollo/client/link/ws'
 import { split, HttpLink } from '@apollo/client'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { useState } from 'react'
-import './App.css'
 import Chats from './components/Chats'
 import { SendMessage } from './components/SendMessage'
-import env from "react-dotenv";
+import env from 'react-dotenv'
 
 const wsLink = new WebSocketLink({
   uri: `ws://localhost:${env.API_PORT}/subscriptions`,
@@ -43,11 +42,11 @@ const client = new ApolloClient({
 function App() {
   const [name, setName] = useState<string>('')
   const [entered, setEntered] = useState<boolean>(false)
-  
+
   return (
     <ApolloProvider client={client}>
-        {!entered && (
-          <section>
+      {!entered && (
+        <section>
           <form>
             <input
               type="text"
@@ -57,16 +56,16 @@ function App() {
             ></input>
             <button onClick={() => setEntered(true)}>Enter chat</button>
           </form>
-          </section>
-        )}
+        </section>
+      )}
 
-        {name !== '' && entered && (
-          <section>
-            <h1>Default Chat Room</h1>
-            <Chats user={name} />
-            <SendMessage name={name} />
-          </section>
-        )}
+      {name !== '' && entered && (
+        <section>
+          <h1>Default Chat Room</h1>
+          <Chats user={name} />
+          <SendMessage name={name} />
+        </section>
+      )}
     </ApolloProvider>
   )
 }
